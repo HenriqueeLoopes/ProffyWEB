bf25f10e5b7c4555bfd4225cfca0d711c0e216a5
Henrique Lopes <henriquegamer83@gmail.com>
2020-08-05 21:20:40 -0300

    new classes form

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,11 +1,44 @@
 import React from "react";
 
+import './styles.css';
+import warningIcon from '../../assets/images/icons/warning.svg';
+
 import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
+import TextArea from "../../components/TextArea";
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="o primeiro passo, e preencher esse formulario de inscricao."
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+          <TextArea name="bio" label="Biografia" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input name="subject" label="Materia" />
+          <Input type="number" name="cost" label="Custa da sua hora por aula" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso Importante"/>
+            Importante <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar cadastro</button>
+        </footer>
+      </main>
     </div>
   );
 }
